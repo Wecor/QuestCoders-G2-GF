127818d78c999ffa16b78705fbcd0a97765de518
Krodriguez11 <rodriguezcastillak@gmail.com>
2024-12-04 18:30:15 -0500

    Configuración del juego: Hecho por Kevin.

--- JuegoAdivinanza.java
+++ JuegoAdivinanza.java
@@ -1,2 +1,42 @@
+import java.util.Random;
+
 public class JuegoAdivinanza {
+    private String palabra;
+    private int intentos;
+    private String progreso;
+
+    public JuegoAdivinanza() {
+        this.palabra = seleccionarPalabraAleatoria().toLowerCase();
+        this.intentos = 40;
+        this.progreso = "_".repeat(palabra.length());
+    }
+
+    private String seleccionarPalabraAleatoria() {
+        String[] palabras = {"casa", "perro", "gato", "comida", "escuela",
+                "amigo", "familia", "trabajo", "carro", "bicicleta",
+                "computadora", "programacion", "telefono", "internet", "musica",
+                "libro", "juego", "pintura", "deporte", "viaje",
+                "ciudad", "montana", "playa", "bosque", "rio",
+                "pajaro", "pez", "sol", "luna", "estrella",
+                "codigo", "bucle", "variable", "clase", "funcion",
+                "pelota", "camisa", "zapato", "silla", "puerta",
+                "reloj", "cocina", "ventana", "fiesta", "avion"};
+        Random random = new Random();
+        return palabras[random.nextInt(palabras.length)];
+    }
+
+    public String getProgreso() {
+
+        return progreso;
+    }
+
+    public int getIntentos() {
+
+        return intentos;
+    }
+
+    public String getPalabra() {
+
+        return palabra;
+    }
 }
